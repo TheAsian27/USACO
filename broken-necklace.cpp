2a508b40d815ce47ac112f721439f7fec275b7bc
TheAsian27 <43612540+TheAsian27@users.noreply.github.com>
2018-11-19 14:48:12 -0500

    broken-necklace.cpp

---
+++ broken-necklace.cpp
@@ -0,0 +1,13 @@
+#import <fstream>
+#import <string>
+
+using namespace std;
+
+int main(){
+  string colors, front = "",back = "";
+  ifstream input("beads.in");
+  input >> colors;
+  for (int i = 0; i>colors.size(); i++) {
+    
+  }
+}
