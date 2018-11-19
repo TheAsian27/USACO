3e26dae6abcba3f1e4ea6a17a690ef140e442d8e
TheAsian27 <43612540+TheAsian27@users.noreply.github.com>
2018-11-19 23:39:20 +0000

    Update broken-necklace.cpp

--- broken-necklace.cpp
+++ broken-necklace.cpp
@@ -4,12 +4,38 @@
 using namespace std;
 
 int main(){
-  string colors, front = "",back = "";
+  string colors;
+  char firstColor,lastColor;
   vector<int> numbOfBeads(colors.size());
   ifstream input("beads.in");
   input >> colors;
   colors = colors + colors
-  for (int i = 0; i>colors.size(); i++) {
-    while (last)
+  for (int i = 0; i<colors.size(); i++) {
+      firstColor = colors.at(i);
+      lastColor = colors.at(i+colors.size());
+      numbOfBeads[i]=2
+    for (int a = 0; a<colors.size(); a++) {
+      if ((color.at(a+i+1)==firstColor)||(color.at(a+i+1)=='w')) {
+        numbOfBeads[i]++;
+      } else {
+        break;
+      }
+    }
+    for (int b= colors.size(); b>0; b--) {
+      if ((color.at(b+i+colors.size()+1)==firstColor)||(color.at(b+i+colors.size()+1)=='w')) {
+        numbOfBeads[i]++;
+      } else {
+        break;
+      }
+    }
   }
+  input.close();
+  for (int i = 1; i<colors; i++) {
+   if (numbOfBeads[0] < numbOfBeads[i]) {
+     numbOfBeads[0] = numbOfBeads[i];
+   }
+  }
+  ofstream output("beads.out");
+  output << numbOfBeads[0];
+  output.close();
 }
