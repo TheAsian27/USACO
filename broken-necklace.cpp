f2e8c7fa73dac69408d1a72b9e20f85d01ca52fd
TheAsian27 <43612540+TheAsian27@users.noreply.github.com>
2018-11-19 15:13:08 -0500

    Update broken-necklace.cpp

--- broken-necklace.cpp
+++ broken-necklace.cpp
@@ -5,9 +5,11 @@ using namespace std;
 
 int main(){
   string colors, front = "",back = "";
+  vector<int> numbOfBeads(colors.size());
   ifstream input("beads.in");
   input >> colors;
+  colors = colors + colors
   for (int i = 0; i>colors.size(); i++) {
-    
+    while (last)
   }
 }
