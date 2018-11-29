54aaaa2ef60a2fc259e8d73c28f18aedca1a3493
TheAsian27 <43612540+TheAsian27@users.noreply.github.com>
2018-11-29 14:49:03 -0500

    Update broken-necklace.cpp

--- broken-necklace.cpp
+++ broken-necklace.cpp
@@ -15,14 +15,34 @@ int main(){
   char firstColor,lastColor;
   vector<int> numbOfBeads(beads);
   int beads = 0;
+  
   ifstream input("beads.in");
   input >> beads;
   input >> colors;
+  input.close();
+  
   colors = colors + colors;
+  
   for (int i = 0; i<beads; i++) {
-      firstColor = colors.at(i);
-      lastColor = colors.at(i+beads);
-      numbOfBeads[i]=2;
+    firstColor = colors.at(i);
+    lastColor = colors.at(i+beads);
+    numbOfBeads[i]=0;
+    if (firstColor=='w') {
+      for (int x = 0; x<beads; x++) {
+        if (colors.at(x+i+1)!='w') {
+          firstColor = colors.at(a+x+1);
+          break;
+        }
+      }
+    }
+    if (lastColor=='w') {
+      for (int y = beads; b>0; b--) {
+        if (colors.at(y+i+beads)!='w')) {
+          lastcolor = colors.at(y+i+beads);
+          break;
+        }
+      }
+    }
     for (int a = 0; a<beads; a++) {
       if ((colors.at(a+i+1)==firstColor)||(colors.at(a+i+1)=='w')) {
         numbOfBeads[i]++;
@@ -31,14 +51,13 @@ int main(){
       }
     }
     for (int b = beads; b>0; b--) {
-      if ((colors.at(b+i+beads)==firstColor)||(colors.at(b+i+beads)=='w')) {
+      if ((colors.at(b+i+beads)==lastColor)||(colors.at(b+i+beads)=='w')) {
         numbOfBeads[i]++;
       } else {
         break;
       }
     }
   }
-  input.close();
   for (int i = 1; i<colors; i++) {
    if (numbOfBeads[0] < numbOfBeads[i]) {
      numbOfBeads[0] = numbOfBeads[i];
