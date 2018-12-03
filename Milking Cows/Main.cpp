91d36e6c4830d3bf75f6575f8d532dd7497a719c
TheAsian27 <43612540+TheAsian27@users.noreply.github.com>
2018-12-03 02:13:26 +0000

    Update Main.cpp

--- Milking Cows/Main.cpp
+++ Milking Cows/Main.cpp
@@ -3,3 +3,27 @@ ID:aaron.l3
 PROB:milk2
 LANG:C++
 */
+
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+struct info {
+  int startTime;
+  int endTime;
+}
+
+int main() {
+  vector<info>farmer;
+  int numbOfFarmers;
+  ifstream input(milk2.in);
+  input >> numbOfFarmers;
+  for (int i = 0; i < numbOfFarmers; i++) {
+    input >> farmer.startTime >> farmer.endTime;
+  }
+  input.close();
+  
+  
+  
+}
