3fe0cd94963c226a5eed46492394d1b8d580e37a
TheAsian27 <43612540+TheAsian27@users.noreply.github.com>
2018-12-04 14:56:50 -0500

    Update NewMilk.cpp

--- Milking Cows/NewMilk.cpp
+++ Milking Cows/NewMilk.cpp
@@ -35,7 +35,7 @@ int main() {
       if (farmer[i].endTime > farmer[j].startTime) {//Checks if the end time is past the start time
         if (farmer[j].endTime > farmer.[i].endTime) {//Checks if new end time is greater than last end time
           farmer[i].endTime = farmer[j].endTime;
-          if (farmer[i].endTime-farmer[i].startTime > maxSqueeze) {
+          if (farmer[i].endTime-farmer[i].startTime > maxSqueeze) {//Sets end time to new end time if it increaces the Squeeze time
             maxSqueeze = farmer[i].endTime-farmer[i].startTime;
           }
         }
@@ -43,10 +43,10 @@ int main() {
         j--;//sets back loop
       }  
     }
-    if (i == 0) {
-      maxNoSqueeze = farmer[i].startTime;
+    if (i == 0) {//Checks If it is the first loop
+      maxNoSqueeze = farmer[i].startTime;//Makes max idle time first squeeze
     } else {
-      if (farmer[i].startTime-farmer[i-1].endTime > maxNoSqueeze) {
+      if (farmer[i].startTime-farmer[i-1].endTime > maxNoSqueeze) {//Checks the space beetween last and first elements of farmers
         maxNoSqueeze = farmer[i].startTime-farmer[i-1].endTime;
       }
     }
