ce0618a17c6e84c9ac15fd8852e948dd510b7c50
TheAsian27 <43612540+TheAsian27@users.noreply.github.com>
2018-12-07 17:00:03 -0500

    Create Main.cpp

---
+++ transform/Main.cpp
@@ -0,0 +1,19 @@
+/*
+ID : aaron.l3
+PROB : transform
+LANG : C++
+*/
+
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int size;
+	ifstream input("transform.in");
+	input >> size;
+	vector<string>(size);
+}
