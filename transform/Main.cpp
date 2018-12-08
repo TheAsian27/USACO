2cc6686ecf5e5cb49be1816984faf248ed3f9d11
TheAsian27 <43612540+TheAsian27@users.noreply.github.com>
2018-12-07 22:56:52 -0500

    Update Main.cpp

--- transform/Main.cpp
+++ transform/Main.cpp
@@ -12,8 +12,26 @@ using namespace std;
 
 int main()
 {
-	int size;
-	ifstream input("transform.in");
-	input >> size;
-	vector<string>(size);
+  int size;
+  ifstream input("transform.in");
+  input >> size;
+  char square [size][size];
+  char transform [size][size];
+  for (int a = 0; a < size; a++) {
+    for (int b = 0; a < size; b++) {
+      input >> square[a][b]    
+    }
+  }
+  for (int a = 0; a < size; a++) {
+    for (int b = 0; a < size; b++) {
+      input >> transform[a][b]    
+    }
+  }
+  input.close();
+  for (int i = 0; i < size; i++) {
+    char temp[size][size];
+    for (int j = 0; i < size; i++) {
+      temp[i][j] = square[][]
+    }
+  }
 }
