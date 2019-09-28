18f5f0649f0c840275e390bb44c26a8bafdce983
TheAsian27 <43612540+TheAsian27@users.noreply.github.com>
2019-09-28 07:41:41 -0400

    Update main.cpp

--- namenum/main.cpp
+++ namenum/main.cpp
@@ -1,1 +1,18 @@
+/*
+ID : aaron.l3
+PROB : namenum
+LANG : C++14
+*/
 
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ifstream nameList("dict.txt");
+    string names[5000];
+    while (!nameList.oef()) {
+        nameList >> names[i];
+    }
+}
