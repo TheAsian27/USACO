0347f5bd9d4bd158613646d46888155f1379f7bc
TheAsian27 <43612540+TheAsian27@users.noreply.github.com>
2019-10-02 13:40:23 -0400

    Update main.cpp

--- namenum/main.cpp
+++ namenum/main.cpp
@@ -4,34 +4,66 @@ PROB : namenum
 LANG : C++14
 */
 
+#define victor vector
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+struct nut {
+	char a, b, c;
+	nut(char _a, char _b, char _c): a(_a), b(_b), c(_c) {}
+};
+
+char yeet(int x, nut) {
+	switch (x) {
+		case 2:
+			nut yeet('A', 'B', 'C');
+			break;
+		case 5 : 
+			nut yeet('J', 'K', 'L');
+			break; 
+		case 8 : 
+			nut yeet('T', 'U', 'V');
+			break;
+		case 3 :
+			nut yeet('D', 'E', 'F');
+			break;
+		case 6 : 
+			nut yeet('M', 'N', 'O');
+			break;
+		case 9 : 
+			nut yeet('W', 'X', 'Y');
+			break;
+		case 4 : 
+			nut yeet('G', 'H', 'I');
+			break; 
+		case 7 :
+			nut yeet('P', 'R', 'S');
+			break;
+		default:
+	}
+}
 
 int main() {
 	string s;
 	string num = "";
 	ifstream nameList("dict.txt");
-	vector<string> names;
+	victor<string> names;
 	while (!nameList.eof()) {
 		nameList >> s;
 		names.push_back(s);
 	}
-	namelist.close();
-    
+	nameList.close();
+
 	ifstream number("namenum.in");
 	number >> s;
 	number.close();
 	num += s;
-    	for (int i = 0; i < names.length; i++) {
-		for (int j = 0; j < number.length; j++) {
-			switch (number[j]) {
-				case 2:
-					for (int k = 0; k < 3; k++) {
-						
-					}
-			}
+	for (int i = 0; i < names.size; i++) {
+		for (int j = 0; j < num.length() ; j++) {
+
 		}
 	}
 }
