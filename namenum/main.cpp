87741f68758bfc7a4e4c551e4543e8b161c85958
TheAsian27 <43612540+TheAsian27@users.noreply.github.com>
2019-10-03 13:36:47 -0400

    Update main.cpp

--- namenum/main.cpp
+++ namenum/main.cpp
@@ -16,31 +16,31 @@ struct nut {
 	nut(char _a, char _b, char _c): a(_a), b(_b), c(_c) {}
 };
 
-char yeet(int x, nut) {
+char yeet(int x) {
 	switch (x) {
 		case 2:
-			nut yeet('A', 'B', 'C');
+			nut letters('A', 'B', 'C');
 			break;
 		case 5 : 
-			nut yeet('J', 'K', 'L');
+			nut letters('J', 'K', 'L');
 			break; 
 		case 8 : 
-			nut yeet('T', 'U', 'V');
+			nut letters('T', 'U', 'V');
 			break;
 		case 3 :
-			nut yeet('D', 'E', 'F');
+			nut letters('D', 'E', 'F');
 			break;
 		case 6 : 
-			nut yeet('M', 'N', 'O');
+			nut letters('M', 'N', 'O');
 			break;
 		case 9 : 
-			nut yeet('W', 'X', 'Y');
+			nut letters('W', 'X', 'Y');
 			break;
 		case 4 : 
-			nut yeet('G', 'H', 'I');
+			nut letters('G', 'H', 'I');
 			break; 
 		case 7 :
-			nut yeet('P', 'R', 'S');
+			nut letters('P', 'R', 'S');
 			break;
 		default:
 	}
@@ -63,7 +63,9 @@ int main() {
 	num += s;
 	for (int i = 0; i < names.size; i++) {
 		for (int j = 0; j < num.length() ; j++) {
-
+			if () {
+			
+			}
 		}
 	}
 }
