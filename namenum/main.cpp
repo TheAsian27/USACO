288f84ea161e8b944433f9ec4be2274ebf8eb9bf
TheAsian27 <43612540+TheAsian27@users.noreply.github.com>
2019-10-11 16:07:35 -0400

    Update main.cpp

--- namenum/main.cpp
+++ namenum/main.cpp
@@ -4,46 +4,78 @@ PROB : namenum
 LANG : C++14
 */
 
-#define victor vector
 #include <fstream>
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
 char yeet(int x, int y) {
-	char c = 59;
-	c += x * 3;
-	c += y;
-	if (c >= 'Q')
-		c--;
+	char a;
+	switch (x) {
+	case 2: 
+		a = 'A';
+		goto finish;
+	case 3:
+		a = 'D';
+		goto finish;
+	case 4:
+		a = 'G';
+		goto finish;
+	case 5:
+		a = 'J';
+		goto finish;
+	case 6:
+		a = 'M';
+		goto finish;
+	case 8:
+		a = 'T';
+		goto finish;
+	case 9:
+		a = 'W';
+		goto finish;
 
-	return c;
+	case 7:
+		a = 'P';
+		if (y > 1) a += y;
+		break;
+
+	finish:
+		a += y - 1;
+	}
+	return a;
 };
 
 int main() {
-	victor<string> names;
+	vector<string> names;
 	string s;
-	ifstream nameList("dict.txt");
-	for (int i = 0; i < 4617; i++) {
-		nameList >> s;
-		names.push_back(s);
-	}
-	nameList.close();
-
-	int num;
+	long long num;
 	unsigned int size = 0;
-	int numNames = 4617;
+	int numNames = 0;
 
 	vector<int> digits;
 	ifstream number("namenum.in");
 	number >> num;
 	number.close();
+
 	while (num > 0) {
 		digits.insert(digits.begin(), (num % 10));
 		num /= 10;
 		size++;
 	}
+
+	ifstream nameList("dict.txt");
+	for (int i = 0; i < 4617; i++) {
+		nameList >> s;
+		if (s.length() == size) {
+			numNames++;
+			names.push_back(s);
+		}
+	}
+	nameList.close();
+
+
 
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < numNames; j++) {
@@ -55,17 +87,15 @@ int main() {
 		}
 	}
 
-	for (int i = 0; i < numNames; i++) {
-		if (names[i].size() > size) {
-			names.erase(names.begin() + i);
-			numNames--;
-			i--;
-		}
-	}
-
 	ofstream output("namenum.out");
-	for (int i = 0; i < numNames; i++) {
-		output << names[i] << "\n";
+	if (numNames == 0) {
+		output << "NONE" << endl;
+	}
+	else {
+		for (int i = 0; i < numNames; i++) {
+			output << names[i] << endl;
+			cout << names[i];
+		}
 	}
 	output.close();
 	return 0;
