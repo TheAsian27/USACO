0ac68c7a4cbee5e16ae272a3e33bcc1728056f7f
TheAsian27 <43612540+TheAsian27@users.noreply.github.com>
2019-10-14 11:35:36 -0400

    Update main.cpp

--- palsquare/main.cpp
+++ palsquare/main.cpp
@@ -18,7 +18,7 @@ using std::ofstream;
 
 std::array<char, 20> correspondingNums = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
 
-inline string baseConverter(int num, const int base) {
+inline string convert(int num, const int base) {
 	string yeet = "";
 	while (num > 0) {
 		yeet += correspondingNums[num % base];
@@ -42,7 +42,7 @@ int main() {
 	ofstream output("palsquare.out");
 	for (int i =1; i <= 300; i++) {
 		if(isPal(convert (base,i*i))){
-			output << i << " " << i*i << endl;
+			output << convert(i) << " " << convert(i*i) << endl;
 		}
 	}
 	output.close()
